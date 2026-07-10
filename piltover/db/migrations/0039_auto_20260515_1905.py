4eba893c40d0a7f4825c58ea788fc69e5e7f6adc
sudopsec <277307031+sudopsec@users.noreply.github.com>
2026-07-10 23:05:18 +0600

    Fix migration 0039 AlterField for MySQL compatibility

--- piltover/db/migrations/0039_auto_20260515_1905.py
+++ piltover/db/migrations/0039_auto_20260515_1905.py
@@ -2,6 +2,15 @@ from tortoise import migrations
 from tortoise.migrations import operations as ops
 from tortoise import fields
 
+
+class AlterFieldStateOnly(ops.AlterField):
+    async def database_forward(self, app_label, old_state, new_state, state_editor=None):
+        return None
+
+    async def database_backward(self, app_label, old_state, new_state, state_editor=None):
+        return None
+
+
 class Migration(migrations.Migration):
     dependencies = [('models', '0038_auto_20260515_1625')]
 
@@ -18,7 +27,11 @@ class Migration(migrations.Migration):
             name='participants_count',
             field=fields.IntField(default=0),
         ),
-        ops.AlterField(
+        ops.RunSQL(
+            "ALTER TABLE `chat` MODIFY COLUMN `participants_count` INT NOT NULL DEFAULT 0",
+            reverse_sql="ALTER TABLE `chat` MODIFY COLUMN `participants_count` SMALLINT NOT NULL",
+        ),
+        AlterFieldStateOnly(
             model_name='Chat',
             name='participants_count',
             field=fields.IntField(default=0),
